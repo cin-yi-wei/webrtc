891f4d71f3db8daed17c3216c02c775010800571
cin-yi-wei <m95162027@gmail.com>
2022-07-18 16:39:35 +0800

    get local video

--- app/javascript/controllers/demo_controller.js
+++ app/javascript/controllers/demo_controller.js
@@ -4,4 +4,16 @@ export default class extends Controller {
   connect() {
     console.log('test')
   }
+
+  getUserMedia() {
+    navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true
+    }).then((stream) => {
+      // this.connection.localStream = stream
+      this.mainTarget.srcObject = stream
+      // this.channel.send({type: "TOKEN"})
+    })
+  }
+
 }
